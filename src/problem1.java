4026d742b233660e491163997ad8eef24ac76d88
Glyn Finck <glynfinck97@gmail.com>
2018-04-16 01:24:43 -0700

    adding problems 1,2 and 3

--- src/problem1.java
+++ src/problem1.java
@@ -1,4 +1,13 @@
-
 public class problem1 {
-
+	public static void main(String [] args) {
+		int n = 1000;
+		int sum = 0;
+		for(int k = 1;k < n;k++) {
+			if(k%3 == 0 || k%5 == 0) {
+				sum = sum + k;
+			}
+		}
+		System.out.println(sum);
+	}
 }
+
---
+++ src/problem2.java
@@ -0,0 +1,19 @@
+
+public class problem2 {
+	public static void main(String [] args) {
+		int prev = 0;
+		int curr = 1;
+		int next = 1;
+		int n = 4000000;
+		int sum = 0;
+		while(next < n) {
+			if(next%2 == 0) {
+				sum = sum + next;
+			}
+			next = prev + curr;
+			prev = curr;
+			curr = next;
+		}
+		System.out.println(sum);
+	}
+}
