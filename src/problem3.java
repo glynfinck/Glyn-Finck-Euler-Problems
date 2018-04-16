2cb28d36f7824bc8526caeca5cdedf1f9c7918e7
Glyn Finck <glynfinck97@gmail.com>
2018-04-16 01:34:00 -0700

    adding final problem3

--- src/problem3.java
+++ src/problem3.java
@@ -1,4 +1,12 @@
 
 public class problem3 {
-
+	public static void main(String [] args) {
+		long n = 600851475143L;
+		for(int i = 2;i<n;i++) {
+			while(n%i == 0){
+				n = n/i;
+			}
+		}
+		System.out.println(n);
+	}
 }
